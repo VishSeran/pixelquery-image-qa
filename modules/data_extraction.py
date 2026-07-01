a36e4d85671d258e7add1baa17cc6d3927b48300
VishSeran <vishwa.sovis@gmail.com>
2026-07-01 12:57:47 +0530

    data extraction updated

--- modules/data_extraction.py
+++ modules/data_extraction.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional,
 from modules.logger import get_logger
 import requests
 import base64
@@ -20,30 +20,40 @@ def get_image_from_url(url):
         logger.error(f"Failed to fetch image: {e}")
         raise
 
-def encode_image(image_path:Optional[str] = None,
-                                url:Optional[str] = None):
+def encode_image(image_paths:Optional[list[str]] = None,
+                                urls:Optional[list[str]] = None):
     
     try:
-        
+        images = []
         image = None
     
         if url:
-            image = get_image_from_url(url).content
+            for url in urls:
+                image = get_image_from_url(url).content
+                images.append(image)
         
-        elif image_path:
-            with open(image_path, "rb") as f:
-                image = f.read()
-        
-        else:
-            raise ValueError("Either image_path or url must be provided.")
+        if image_paths:
+            for image_path in image_paths:
+                with open(image_path, "rb") as f:
+                    image = f.read()
+                    images.append(image)
+            
+        if not images:
+            raise ValueError("Provide at least one image path or URL.")
         
         # encode raw bytes into bytes object and again decode bytes object into python string
-        encoded_image = base64.b64encode(image).decode("utf-8")
+        encoded_image = [
+            base64.b64encode(img).decode("utf-8")
+            for img in images
+        ]
         
         return encoded_image
     
     except ValueError as e:
-        print(f"Value error: {e}")
-        
+        logger.error(f"Value error: {e}")
+        raise
+    
     except Exception as e:
-        print(f"Error in image encoding: {e}")
+        logger.error(f"Multi-image encoding failed: : {e}")
+        raise
+        
