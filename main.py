d62bffe92a6485f58842332c6d0066ad38e16b2a
VishSeran <vishwa.sovis@gmail.com>
2026-07-01 19:36:29 +0530

    main updated

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+from typing import Optional
+
 import gradio as gr
 
-def image_caption_process(image, user_query):
+from modules.data_extraction import encode_image
+from modules.logger import get_logger
+from modules.model_config import get_response_from_model
+
+logger = get_logger("main")
+
+def image_caption_process(image_path:Optional[str],image_url:Optional[str], user_query):
     
     try:
+        image = encode_image(image_path, image_url)
+        response = get_response_from_model(image, user_query)
         
+        return response
     except ValueError as e:
         logger.error(f"Value error: {e}")
         raise
     
     except Exception as e:
-        logger.error(f"user query fetch error: : {e}")
+        logger.error(f"main function error: : {e}")
         raise 
 
 def gradio_interface():
@@ -21,5 +32,14 @@ def gradio_interface():
     text_output = gr.Textbox(label="Image caption")
     
     interface = gr.Interface(
-        fn=
+        fn=image_caption_process,
+        inputs=[image_input, input_url,user_query],
+        outputs=[text_output],
+        title="Image Captioning AI",
+        description="This application is use to generate a caption for images"
     )
+    
+    interface.launch()
+    
+if __name__ == "__main__":
+    gradio_interface()
